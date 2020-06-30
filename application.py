7e05544c5a33f50c6961b79360581ca53ff24adf
abhishek-goyal <45648611+Abhishek445566@users.noreply.github.com>
2020-06-30 21:45:50 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,11 +6,4 @@ app=Flask(__name__)
   def index():
     return render_template("index.html")
     
-@app.route("/hello",method=["POST"])
-  def hello():
-    name = request.form.get("name")
-    return render_template("hello.html",name=name)
 
-@app.route("/more")
-  def more():
-      return render_template("more.html")
